fbe982ba3e9fc0cc4886ba60d014c41256d72a01
luizmarcelloxamarin <luiztelemicro@hotmail.com>
2020-06-15 23:21:49 -0300

    Umas alterações_1

---
+++ AppXamarinFormsLayouts/AppXamarinFormsLayouts/AppXamarinFormsLayouts/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppXamarinFormsLayouts
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new StackLayoutPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
