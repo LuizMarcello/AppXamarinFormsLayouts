7e3bd33651aba37f7d539e64c563a50f7448b230
luizmarcelloxamarin <luiztelemicro@hotmail.com>
2020-06-16 22:24:07 -0300

    Uma alteração_02

--- AppXamarinFormsLayouts/AppXamarinFormsLayouts/AppXamarinFormsLayouts/StackLayoutPage.xaml.cs
+++ AppXamarinFormsLayouts/AppXamarinFormsLayouts/AppXamarinFormsLayouts/StackLayoutPage.xaml.cs
@@ -15,6 +15,69 @@ namespace AppXamarinFormsLayouts
         public StackLayoutPage()
         {
             InitializeComponent();
+
+            //1ª maneira de fazer com código c#:
+            //var layout = new StackLayout
+            //{
+            //    BackgroundColor = Color.Silver,
+            //    Spacing = 20,
+            //    Padding = new Thickness(10, 20, 10, 10),
+            //    Orientation = StackOrientation.Vertical
+            //};
+
+            //layout.Children.Add(new Label { Text = "Label 1", BackgroundColor = Color.Aqua });
+            //layout.Children.Add(new Button { Text = "Button 1", BackgroundColor = Color.Aqua });
+
+            //Content = layout;
+
+            //2ª maneira de fazer com código c#:
+            StackLayout stackLayout = new StackLayout
+            {
+                BackgroundColor = Color.Yellow,
+                Spacing = 20,
+                Padding = new Thickness(10, 20, 10, 10),
+                Orientation = StackOrientation.Vertical,
+                VerticalOptions = LayoutOptions.Center,
+
+                Children =
+                {
+                    new Label
+                    {
+                        Text="Xamarin Forms",
+                        BackgroundColor=Color.Silver,
+                        HorizontalOptions = LayoutOptions.Start
+                    },
+
+                    new Label
+                    {
+                        Text="Macoratti.net",
+                        BackgroundColor=Color.Silver,
+                        HorizontalOptions = LayoutOptions.Center
+                    },
+
+                    new StackLayout
+                    {
+                        BackgroundColor = Color.Silver,
+                        Spacing = 10,
+                        Padding = new Thickness(30),
+                        Orientation = StackOrientation.Horizontal,
+                        HorizontalOptions = LayoutOptions.Center,
+
+                        Children =
+                        {
+                            new Label
+                            {
+                                Text="Label 1",
+                                BackgroundColor=Color.Red,
+                            }
+                        }
+                    }
+                },
+            };
+
+            Content = stackLayout;
         }
     }
 }
+
+
